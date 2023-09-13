b76fc6edacfa5024ee31ea351e40af11978e82d6
Yukino0113 <jethro0113@gmail.com>
2023-09-13 20:44:56 +0800

    Complete bot register function

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,6 +1,9 @@
 import os
+import time
+
 import discord
 from dotenv import load_dotenv
+from maimai.user_data_fetch.add_friend import add_friend
 
 load_dotenv()
 
@@ -8,8 +11,13 @@ bot = discord.Bot()
 
 
 @bot.command(description="Maimai")
-async def maimai_bot_register(ctx):
-    await ctx.respond(f"Hello {ctx.author.name}!")
+async def maimai_bot_register(ctx, friend_code: str):
+    await ctx.respond('處理中，請稍後...')
+    await ctx.send(f'{ctx.user.mention} \n {add_friend(friend_code)}')
 
+
+@bot.command(description="Testt")
+async def t(ctx):
+    await ctx.respond(f'{ctx.user.mention}')
 
 bot.run(os.getenv("TOKEN"))
