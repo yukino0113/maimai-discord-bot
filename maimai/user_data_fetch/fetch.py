c16b01f36463317eee4d3719a73500505c9fd839
Yukino0113 <jethro0113@gmail.com>
2023-09-14 10:18:23 +0800

    Create class for mai net, will merge fetch and add friends

--- maimai/user_data_fetch/fetch.py
+++ maimai/user_data_fetch/fetch.py
@@ -1,5 +1,5 @@
 import os
-
+from maimai.user_data_fetch.user import User
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import warnings
@@ -16,9 +16,36 @@ url = f'https://maimaidx-eng.com/maimai-mobile/friend/friendGenreVs/battleStart/
 """
 
 
+class MaimaiNet:
+
+    def __init__(self):
+        option = webdriver.EdgeOptions()
+        # option.headless = True
+        self.driver = webdriver.Edge(option)
+        self.driver.get('https://maimaidx-eng.com/maimai-mobile/home/')
+        self.login()
+
+    def __login(self):
+        self.driver.find_element(By.XPATH,
+                                 "//section[@id='segaid']//span[@class='c-button--openid--segaId']").click()
+        self.driver.find_element(By.NAME, 'sid').send_keys(os.getenv("ACCOUNT"))
+        self.driver.find_element(By.NAME, 'password').send_keys(os.getenv("PW"))
+        self.driver.find_element(By.ID, 'btnSubmit').click()
+
+    def add_friends(self):
+        pass
+
+    def fetch_song_score(self):
+        pass
+
+    def fetch_user_information(self):
+        pass
+
+
+
 def open_browser():
     option = webdriver.EdgeOptions()
-    #option.headless = True
+    # option.headless = True
     driver = webdriver.Edge(option)
     driver.get('https://maimaidx-eng.com/maimai-mobile/home/')
 
@@ -31,14 +58,14 @@ def open_browser():
 
     return driver
 
-def fetch_song_score():
-    #driver = open_browser()
+
+def fetch_song_score(user: User):
+    # driver = open_browser()
+    # todo: add to favorites
     for i in range(0, 5):
         print(i)
-
 
 
 def fetch_user_information(friend_code):
     driver = open_browser()
     driver.get(f'https://maimaidx-eng.com/maimai-mobile/friend/friendDetail/?idx={friend_code}')
-
