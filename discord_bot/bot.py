ed8d0aaf236e9f9a6ade6de4f7ecd09833e47199
Yukino0113 <jethro0113@gmail.com>
2023-09-14 10:17:43 +0800

    Initial added get user information command

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,6 +1,4 @@
 import os
-import time
-
 import discord
 from dotenv import load_dotenv
 from maimai.user_data_fetch.add_friend import add_friend
@@ -13,11 +11,17 @@ bot = discord.Bot()
 @bot.command(description="Maimai")
 async def maimai_bot_register(ctx, friend_code: str):
     await ctx.respond('處理中，請稍後...')
-    await ctx.send(f'{ctx.user.mention} \n {add_friend(friend_code)}')
+    await ctx.send(f'{ctx.user.mention} \n {add_friend(ctx.user, friend_code)}')
+
+
+@bot.command(description="獲取特定玩家資料")
+async def get_user_data(ctx, friend_code: str = ''):
+    await ctx.respond('處理中，請稍後...')
 
 
 @bot.command(description="Testt")
 async def t(ctx):
     await ctx.respond(f'{ctx.user.mention}')
+
 
 bot.run(os.getenv("TOKEN"))
