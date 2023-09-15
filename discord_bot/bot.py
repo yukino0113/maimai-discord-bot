67e55fc90778ac2c4adf75e087e69cfa5c7ee8c2
Yukino0113 <jethro0113@gmail.com>
2023-09-15 08:31:21 +0800

    Added Show user in-game name to check if account is correct

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
-from maimai.user_data_fetch.add_friend import add_friend
+from maimai.user_data_fetch.fetch import MaimaiNet
 
 load_dotenv()
 
@@ -9,9 +9,9 @@ bot = discord.Bot()
 
 
 @bot.command(description="Maimai")
-async def maimai_bot_register(ctx, friend_code: str):
+async def maimai_bot_register(ctx, friend_code):
     await ctx.respond('處理中，請稍後...')
-    await ctx.send(f'{ctx.user.mention} \n {add_friend(ctx.user, friend_code)}')
+    await ctx.send(f'{ctx.user.mention} \n {MaimaiNet().add_friends(ctx.user.name, friend_code)}')
 
 
 @bot.command(description="獲取特定玩家資料")
@@ -21,7 +21,7 @@ async def get_user_data(ctx, friend_code: str = ''):
 
 @bot.command(description="Testt")
 async def t(ctx):
-    await ctx.respond(f'{ctx.user.mention}')
+    await ctx.respond(f'{ctx.user.name}')
 
 
 bot.run(os.getenv("TOKEN"))
